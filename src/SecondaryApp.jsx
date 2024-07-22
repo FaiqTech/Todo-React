514ca89daac26e057a60fa612b4f8d973721c304
solmazmry <mirzayevasolmaz2@gmail.com>
2024-07-22 15:10:25 +0400

    first commit

---
+++ src/SecondaryApp.jsx
@@ -0,0 +1,16 @@
+// // function componet 
+// import React from "react"
+// class SecondaryApp extends React.Component{
+//     state={
+//       count:0  
+//     }
+// // render sonra retrun yaziriq 
+//    render(){
+//     return <div>
+//         <button onClick={()=>this.setState(prevState=>({count:prevState.count+1}))}>Artir</button>
+//         <p>{this.state.count}</p>
+//         <button onClick={()=>this.setState(prevState=>({count:prevState.count--}))}>Azalt</button>
+//          </div>
+//    }
+// }
+// export default SecondaryApp
